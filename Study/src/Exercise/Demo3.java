ce7a50f1bdd47f7938d4af5d9cf3a641a3b6a73c
76365 <763654049@qq.com>
2020-06-13 21:33:11 +0800

    Firs Push

--- Study/src/Exercise/Demo3.java
+++ Study/src/Exercise/Demo3.java
@@ -1,4 +1,16 @@
 package Exercise;
 
 public class Demo3 {
+    public static void main(String[] args) {
+        Integer[] a = {1, 2, 3};
+        Integer[] b = a;
+
+        try {
+            int s = 1 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
