7da4d777cbeb2fa6afc33b8a93ce12b04a3ba91f
76365 <763654049@qq.com>
2020-07-14 16:40:15 +0800

    commit

--- Study/src/Exercise/Sub.java
+++ Study/src/Exercise/Sub.java
@@ -1,11 +1,22 @@
 package Exercise;
 
 public class Sub {
-    public Long getLenght() {
-        return new Long(5);
+    public static String getDimName(String string) {
+        String newstring = new String();
+        if (null == string) {
+            return "";
+        }
+        StringBuffer stringbuffer = new StringBuffer();
+        for (int i = 0; i < string.length() - 1; i++) {
+            stringbuffer.append("*");
+        }
+        stringbuffer.append(string.subSequence(string.length() - 1, string.length()));
+        newstring = stringbuffer.toString();
+
+        return newstring;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(getDimName("保剑"));
     }
 }
